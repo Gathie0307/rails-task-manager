93079403073957747d7e8bef27806415b4cd7a91
Gayathri Krishnan <iamkgayu@gmail.com>
2024-09-25 23:56:47 +0100

    changes to update and delete

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -17,6 +17,22 @@ class TasksController < ApplicationController
     redirect_to index_path
   end
 
+  def edit
+    @tasks = Task.find(params[:id])
+  end
+
+  def update
+    @tasks = Task.find(params[:id])
+    @tasks.update(task_params)
+    redirect_to task_path
+  end
+
+  def destroy
+    @tasks = Task.find(params[:id])
+    @tasks.destroy
+    redirect_to index_path
+  end
+
   private
   def task_params
     params.require(:task).permit(:title, :details)
