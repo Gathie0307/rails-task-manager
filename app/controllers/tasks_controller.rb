164754e638c8e3f7667b4c1c5b6e36b050207b04
Gayathri Krishnan <iamkgayu@gmail.com>
2024-09-25 12:29:27 +0100

    Rails Task Manager

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,4 +6,19 @@ class TasksController < ApplicationController
   def show
     @tasks = Task.find(params[:id])
   end
+
+  def new
+    @tasks = Task.new
+  end
+
+  def create
+    @tasks = Task.new(task_params)
+    @tasks.save
+    redirect_to index_path
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:title, :details)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,7 @@ Rails.application.routes.draw do
   # root "posts#index"
 
   get "tasks", to: "tasks#index", as: :index
+  get "tasks/new", to: "tasks#new"
   get "tasks/:id", to: "tasks#show", as: :task
+  post "tasks", to: "tasks#create"
 end
